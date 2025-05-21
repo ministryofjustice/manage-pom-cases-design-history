1d396c2a88800b3d5bcb23ca7f231f4ccea6a980
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-05-21 15:38:22 +0100

    patch

--- etc/rollup.config.js
+++ etc/rollup.config.js
@@ -1,20 +1,22 @@
-const { nodeResolve } = require('@rollup/plugin-node-resolve')
-const commonjs = require('@rollup/plugin-commonjs')
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
 
-module.exports = [{
-  input: 'app/_javascripts/application.js',
-  output: {
-    file: 'public/javascripts/application.js',
-    format: 'iife'
+module.exports = [
+  {
+    input: "app/_javascripts/application.js",
+    output: {
+      file: "public/javascripts/application.js",
+      format: "iife",
+    },
+    plugins: [nodeResolve(), commonjs()],
   },
-  plugins: [
-    nodeResolve(),
-    commonjs()
-  ]
-}, {
-  input: 'node_modules/govuk-frontend/govuk/all.js',
-  output: {
-    file: 'public/javascripts/govuk-frontend.js'
+  {
+    input: "node_modules/govuk-frontend/dist/govuk/all",
+    output: {
+      file: "public/javascripts/govuk-frontend.js",
+      format: "iife",
+    },
+    context: "window",
+    plugins: [nodeResolve(), commonjs()],
   },
-  context: 'window'
-}]
+];
